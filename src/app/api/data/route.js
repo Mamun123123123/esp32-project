3b83454a31f349c396a94f6d63900d96f2b2076a
Mamun123123123 <almalm90459@gmail.com>
2026-05-11 21:56:58 +0600

    googlesheet setup

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -1,22 +1,41 @@
-let sensorData = {
-  temperature: 0,
-  humidity: 0
-};
+import { google } from "googleapis";
+
+let sensorData = { temperature: 0, humidity: 0 };
+
+const auth = new google.auth.JWT(
+  process.env.GOOGLE_CLIENT_EMAIL,
+  null,
+  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  ["https://www.googleapis.com/auth/spreadsheets"]
+);
+
+const sheets = google.sheets({ version: "v4", auth });
+
+const SPREADSHEET_ID = process.env.SHEET_ID;
 
 export async function POST(req) {
-
   const body = await req.json();
 
   sensorData = body;
 
-  console.log(sensorData);
-
-  return Response.json({
-    message: "Data received"
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: SPREADSHEET_ID,
+    range: "SensorData!A:C",
+    valueInputOption: "USER_ENTERED",
+    requestBody: {
+      values: [
+        [
+          new Date().toLocaleString(),
+          body.temperature,
+          body.humidity
+        ]
+      ]
+    }
   });
+
+  return Response.json({ message: "Saved to Sheet" });
 }
 
 export async function GET() {
-
   return Response.json(sensorData);
 }
